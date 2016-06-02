7cf55489a780cfa453060d0afb0b8419bfcd2d66
cybergla <tanaydeshmukh@gmail.com>
2016-06-02 13:06:47 +0530

    Added prediction, output of predicted items

--- property_clustering.py
+++ property_clustering.py
@@ -1,11 +1,10 @@
 import numpy as np
 import pandas as pd
 from sklearn import cluster
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 
 names = ['pid', 'name', 'covered_area', 'lease_duration_expected', 'lat', 'lon', 'lock_in_period', 'maintenance_charges', 'no_of_balconies', 'no_of_barooms', 'plot_area', 'rent_expected', 'security_deposit', 'type_of_accomodation', 'total_floors', 'floor', 'servant_room', 'servant_room_wi_toilet']
 df = pd.read_csv('data.csv',sep=',',header=1,names=names)
-
 n_samples = df.pid.unique().shape[0]
 n_features = len(names)
 
@@ -17,10 +16,21 @@ for row in df.itertuples():
 	for x in xrange(0,n_features-3):
 		dataset[row[0],x] = row[x+3]
 
-k_means = cluster.KMeans(n_clusters=8)
-k_means.fit(dataset)
-labels = k_means.labels_
-centroids = k_means.cluster_centers_
+train = dataset.copy()
+train = train[10:]
+test = dataset.copy()
+test = test[:10]
 
-print labels
-print centroids
+k_means = cluster.KMeans(n_clusters=10)
+k_means.fit(train)
+
+labels = k_means.labels_
+#print len(labels),dataset.shape,train.shape,test.shape
+clusters = k_means.predict(test)
+for j in xrange(len(test)):
+	similar = []
+	for i in xrange(len(labels)):
+		if labels[i] == clusters[j]:
+			similar.append(i+10)
+	outf = df.ix[similar]
+	outf.to_csv('output'+str(j)+'.csv',columns=names)
