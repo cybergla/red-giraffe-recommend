45698205a1e650a65f079fba79dee2922cafb2c4
cybergla <tanaydeshmukh@gmail.com>
2016-06-06 16:45:52 +0530

    Improved preprocessing, added comments

--- predict.py
+++ predict.py
@@ -5,30 +5,30 @@ from sklearn import cluster
 from sklearn.externals import joblib
 from sklearn import preprocessing
 
-FILE_DATA = "input_data.csv"
-FILE_FEATURES = "features.csv"
+FILE_DATA = "input_data.csv"	#File to get the test data from
+FILE_FEATURES = "features.csv"	#File to get the feature list from
 
 df_features = pd.read_csv(FILE_FEATURES,sep=',',header=None)
 selected_columns = df_features[0].values
 
 df = pd.read_csv(FILE_DATA,sep=',',header=0,usecols=selected_columns)
-n_samples = df._id.shape[0]
-n_features = len(selected_columns)
 
+#Data preprocessing
 for index, row in df.iterrows():
 	df.loc[index,"_source/type_of_accomodation"] = df.loc[index,"_source/type_of_accomodation"].replace("bhk","").replace("rk","")
 	df.loc[index,"_source/floor"] = re.sub(r"\D*","",df.loc[index,"_source/floor"])
 
+#Load scale model from disk
 std_scale = joblib.load('scale_model.pkl')
+#Normalize data
 test = std_scale.transform(df[selected_columns[2:]])
+#Load cluster model from disk
 model = joblib.load('cluster_model.pkl')
+#Get labels of cluster model
 labels = model.labels_
-#print labels, labels.shape
-#print len(labels),dataset.shape,dataset.shape,test.shape
+#Predict cluster no. of test data
 clusters = model.predict(test)
-print clusters
-
-df_in = pd.read_csv("data.csv",sep=',',header=0)
+df_in = pd.read_csv("result.csv",sep=',',header=0)
 
 for j in xrange(len(test)):
 		similar = []
--- property_clustering.py
+++ property_clustering.py
@@ -1,26 +1,40 @@
 import numpy as np
 import pandas as pd
+import re
 from sklearn import cluster
 from sklearn.externals import joblib
 from sklearn import preprocessing
 
-FILE_DATA = "data.csv"
-FILE_FEATURES = "features.csv"
+FILE_DATA = "result.csv"	#File to get the training data from
+FILE_FEATURES = "features.csv"	#File to get the feature list from
 
-df_features = pd.read_csv(FILE_FEATURES,sep=',',header=None)
-selected_columns = df_features[0].values
+df_features = pd.read_csv(FILE_FEATURES,sep=',',header=None)	
+selected_columns = df_features[0].values	#Only select these features
 
 df = pd.read_csv(FILE_DATA,sep=',',header=0, usecols=selected_columns)
+#Remove rows with NaN, null values
+df = df.dropna()	
 
-#TODO: preprocessing input, validation
+#Preprocess data
+for index, row in df.iterrows():
+	#Replace bhk, rk etc. in type of accomodation
+	df.loc[index,"_source/type_of_accomodation"] = re.sub(r"\D*","",df.loc[index,"_source/type_of_accomodation"])
+	#Replace floor desc with number
+	df.loc[index,"_source/floor"] = re.sub(r"Duplex|Ground_\d|Ground",'0',df.loc[index,"_source/floor"])
+	df.loc[index,"_source/floor"] = re.sub(r"\D*","",df.loc[index,"_source/floor"])
 
+#Normalize data
 std_scale = preprocessing.StandardScaler().fit(df[selected_columns[2:]])
-joblib.dump(std_scale,'scale_model.pkl')
 dataset = std_scale.transform(df[selected_columns[2:]])
 
+#Determine number of clusters
 n_clusters = dataset.shape[0]/5
+#Make model
 model = cluster.MiniBatchKMeans(init='k-means++', n_clusters=n_clusters, batch_size=100, n_init=10, max_no_improvement=10, verbose=0, random_state=0)
 model.fit(dataset)
 
 #Save model to disk
 joblib.dump(model, 'cluster_model.pkl')
+#Save normalized model to disk
+joblib.dump(std_scale,'scale_model.pkl')
+
