eec1104ef806f43c320b9543f1cd09f1b907d0f5
umer_1201 <umer@exsete.com>
2016-06-09 14:08:13 +0530

    alterd server.py

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from pandas.io.json import json_normalize
-
+'''
+curl -H "Content-type: application/json" -X POST http://127.0.0.1:5000/predict/api/v1/get-prediction/ -d ''
+'''
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -11,6 +13,12 @@ app = Flask(__name__)
 
 @app.route('/predict/api/v1/get-prediction/',methods=['POST'])
 def return_predictions():
-	data = request.json
-	result = json_normalize(data['hits']['hits'])
-	return jsonify(predict.get_reccomended_ids(result))
+	if request.headers['Content-Type'] == 'text/plain':
+		return "Text Message: " 
+        elif request.headers['Content-Type'] == 'application/json':
+		data = request.json
+		result = json_normalize(data['hits']['hits'])
+		return jsonify(predict.get_reccomended_ids(result))
+
+	else:
+		return "415 Unsupported Media Type ;)"
