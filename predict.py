c51f6a8977fdcf2fa295cfdeb4ca1e1dd0f997bc
umer_1201 <umer@exsete.com>
2016-06-23 12:18:20 +0530

    odified preprocess to remove invalid values

--- predict.py
+++ predict.py
@@ -35,6 +35,8 @@ def get_reccomended_ids(df,FILE_INDEX=constants.FILE_INDEX,header=None,getList=T
 	else:
 		return df_in.ix[similar]
 
+#print get_reccomended_ids(pd.read_csv('input_data.csv'))#.to_csv('results.csv')
+
 if (DEBUG and __name__ == '__main__'):
 	print get_reccomended_ids(pd.read_csv('./data/input_data2.csv'))
 
